8e5395009d37ca6ec1b1e2f6d4fd3b5167934613
hg222dt <hg222dt@student.lnu.se>
2014-03-31 15:26:28 +0200

    har fixat till url till editComment
    samt andra commentars-sidor

--- LectureComments/LectureComments/App_Start/RouteConfig.cs
+++ LectureComments/LectureComments/App_Start/RouteConfig.cs
@@ -13,21 +13,21 @@ namespace LectureComments.App_Start
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.MapPageRoute("LectureListing",   "forelasningar",                        "~/Pages/RegLecturePages/Listing2.aspx");
-            routes.MapPageRoute("CreateLecture",    "forelasningar/ny",                     "~/Pages/RegLecturePages/CreateLecture2.aspx");
-            routes.MapPageRoute("DetailsLecture",   "forelasningar/{id}",                   "~/Pages/RegLecturePages/LectureDetails2.aspx");
-            routes.MapPageRoute("EditLecture",      "forelasning/{id}/redigera",            "~/Pages/RegLecturePages/EditLecture2.aspx");
-            routes.MapPageRoute("DeleteLecture",    "forelasning/{id}/radera",              "~/Pages/RegLecturePages/Delete2.aspx");
-            routes.MapPageRoute("AddToThread",      "forelasning/{id}/fraga",               "~/Pages/RegLecturePages/StartThread2.aspx");
-            routes.MapPageRoute("AddComment",       "forelasning/kommentera/{id}/forelasning/{id2}",          "~/Pages/RegLecturePages/AddComment2.aspx");
-            routes.MapPageRoute("EditComment",      "forelasning/kommentera/uppdatera/{id}/forelasning/{id2}","~/Pages/RegLecturePages/EditComment2.aspx");
-            routes.MapPageRoute("DeleteComment",    "forelasning/kommentera/radera/{id}forelasning/{id2}",   "~/Pages/RegLecturePages/DeleteComment2.aspx");
+            routes.MapPageRoute("LectureListing",   "forelasningar",                                    "~/Pages/RegLecturePages/Listing2.aspx");
+            routes.MapPageRoute("CreateLecture",    "forelasning/ny",                                   "~/Pages/RegLecturePages/CreateLecture2.aspx");
+            routes.MapPageRoute("DetailsLecture",   "forelasning/{id}",                                 "~/Pages/RegLecturePages/LectureDetails2.aspx");
+            routes.MapPageRoute("EditLecture",      "forelasning/{id}/redigera",                        "~/Pages/RegLecturePages/EditLecture2.aspx");
+            routes.MapPageRoute("DeleteLecture",    "forelasning/{id}/radera",                          "~/Pages/RegLecturePages/Delete2.aspx");
+            routes.MapPageRoute("AddToThread",      "forelasning/{id}/fraga",                           "~/Pages/RegLecturePages/StartThread2.aspx");
+            routes.MapPageRoute("AddComment",       "forelasning/{id2}/kommentar/{id}/ny",              "~/Pages/RegLecturePages/AddComment2.aspx");
+            routes.MapPageRoute("EditComment",      "forelasning/{id2}/kommentar/{id}/uppdatera",       "~/Pages/RegLecturePages/EditComment2.aspx");
+            routes.MapPageRoute("DeleteComment",    "forelasning/{id2}/kommentar/{id}/radera",          "~/Pages/RegLecturePages/DeleteComment2.aspx");
 
-            routes.MapPageRoute("ListTest",         "forelasningar/test/{id}",              "~/Pages/RegLecturePages/WebForm2.aspx");
+            routes.MapPageRoute("ListTest",         "forelasningar/test/{id}",                          "~/Pages/RegLecturePages/WebForm2.aspx");
 
-            routes.MapPageRoute("Error",            "serverfel",                            "~/Pages/Shared/Error2.aspx");
+            routes.MapPageRoute("Error",            "serverfel",                                        "~/Pages/Shared/Error2.aspx");
             
-            routes.MapPageRoute("Default",          "",                                     "~/Pages/RegLecturePages/Listing2.aspx");
+            routes.MapPageRoute("Default",          "",                                                 "~/Pages/RegLecturePages/Listing2.aspx");
         }
     }
 }
